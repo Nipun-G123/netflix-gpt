c52b89b8e63fee253e33fb72970e97f5048b4ea3
Nipun Kumar Gupta <nipun.gupta@indiamart.com>
2025-02-09 13:29:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+            <h1 className='bg-amber-400 text-4xl'>App component loaded</h1>
+        </>
+    )
+}
+
+export default App
